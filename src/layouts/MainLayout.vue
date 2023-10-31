0ce1a7b71b6c8887da7d76e8730ece950b51fa9d
BETUELLJB <92798309+BETUELLJB@users.noreply.github.com>
2023-10-31 23:43:44 +0200

    aa

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,115 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+      </q-toolbar>
+
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Paiva</div>
+        <div class="text-subtitle1">Paiva</div>
+        <div class="text-h5">{{ todaysDate }}</div>
+      </div>
+
+      <q-img src="src\assets\img\py.jpg"  class="header-image absolute-top" />
+
+    </q-header>
+
+    <q-drawer
+
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="200"
+      :breakpoint="400"
+    >
+      <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+        <q-list padding>
+          <q-item to="/" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="inbox" />
+            </q-item-section>
+
+            <q-item-section>
+              Casa
+            </q-item-section>
+          </q-item>
+          <q-item to="/help" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section>
+              Ajuda
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img class="absolute-top" src="src\assets\img\py1.jpg" style="height: 150px">
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="src\assets\img\py.jpg">
+          </q-avatar>
+          <div class="text-weight-bold">Paiva</div>
+          <div>José</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+import { date } from 'quasar'
+
+const linksList = [
+
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  },
+  computed: {
+    todaysDate () {
+      const timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd  D  MMMM  ')
+    }
+  }
+})
+</script>
+
+<style image="scss">
+.header-image{
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <q-page class="container">
+    <h4 class="q-mt-none p-pa-lg ">Ajuda!</h4>
+  </q-page>
+</template>
